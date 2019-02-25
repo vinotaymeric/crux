371c48044e9dc697760f2c6f286444f47bbd0300
nicolas <nicolas.eymenier@gmail.com>
2019-02-25 14:45:47 +0100

    routes created

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
-  get 'itineraries/index'
-  get 'itineraries/show'
+
+  get 'pages/home'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'pages#home'
+
+  resources :itineraries, only: [:index, :show] do
+    resources :trips, only: [:new, :create]
+  end
+
+  resources :trips, only: [:show, :index, :edit, :update]
+
+
 end
