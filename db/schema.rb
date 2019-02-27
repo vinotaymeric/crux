c62026cb324230759d9244aa497fb2451d3000b6
Aymeric Vinot <aymeric.vinot@gmail.com>
2019-02-27 17:28:26 +0100

    t

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_27_140234) do
+ActiveRecord::Schema.define(version: 2019_02_26_151910) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,8 +71,6 @@ ActiveRecord::Schema.define(version: 2019_02_27_140234) do
     t.string "stability"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.float "x_lat"
-    t.float "y_lat"
   end
 
   create_table "trips", force: :cascade do |t|
