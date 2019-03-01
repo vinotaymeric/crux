896e2fed7456cba4aae1933ca4b426c01826c287
Aymeric Vinot <aymeric.vinot@gmail.com>
2019-03-01 16:52:39 +0100

    css card

--- app/controllers/basecamps_activities_controller.rb
+++ app/controllers/basecamps_activities_controller.rb
@@ -20,7 +20,7 @@ class BasecampsActivitiesController < ApplicationController
     @activity = @basecamp_activity.activity
     @mountain_range = @basecamp.mountain_range
     user_level_for_activity = current_user.user_activities.find_by(activity_id: @activity.id).level
-    @itineraries = @basecamp_activity.itineraries.where(level: user_level_for_activity )
+    @itineraries = @basecamp_activity.itineraries.where(level: user_level_for_activity )[0..20]
   end
 
   private
