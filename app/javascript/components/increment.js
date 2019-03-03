0f1e291807cb8b40f1b8cfd156557e6e6d962559
nicolas <nicolas.eymenier@gmail.com>
2019-03-03 17:37:11 +0100

    transition

--- app/javascript/components/increment.js
+++ app/javascript/components/increment.js
@@ -1,5 +1,22 @@
 import $ from 'jquery';
 
+const button = document.querySelector('#test-transition')
+const boussole = document.querySelector('.boussole')
+
+
+
+
+const initTransition = () => {
+  if (button == null) {
+    return ;
+  }
+    button.addEventListener('click', (e) => {
+      console.log("toto1")
+      boussole.classList.add("tourne");
+
+});
+}
+
 const incrementor = () => {
   $('.count').each(function () {
     $(this).prop('Counter',0).animate({
@@ -14,4 +31,6 @@ const incrementor = () => {
 });
 };
 
-export default incrementor;
+
+// export default initTransition;
+export { initTransition, incrementor};
