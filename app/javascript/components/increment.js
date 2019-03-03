d70b70e2e7149aa6bd5bdd1172b6edee3c910e2e
nicolas <nicolas.eymenier@gmail.com>
2019-03-03 21:22:13 +0100

    transition aux petits oignons

--- app/javascript/components/increment.js
+++ app/javascript/components/increment.js
@@ -5,7 +5,9 @@ const boussole = document.querySelector('.boussole')
 const curtain = document.querySelector('.curtain')
 const footer = document.querySelector('.page-footer')
 const greenButton = document.querySelector('#new_trip')
-const list = document.querySelector('.list')
+const rowIti = document.querySelector('#row-iti')
+const rowMassif = document.querySelector('#row-massif')
+const rowBasecamp = document.querySelector('#row-basecamp')
 
 function sleep(milliseconds) {
   var start = new Date().getTime();
@@ -17,12 +19,12 @@ function sleep(milliseconds) {
 }
 
 
-const incrementor = () => {
-  $('.count').each(function () {
+const incrementor = (id) => {
+  $(id).each(function () {
     $(this).prop('Counter',0).animate({
         Counter: $(this).text()
     }, {
-        duration: 12000,
+        duration: 4000,
         easing: 'swing',
         step: function (now) {
             $(this).text(Math.ceil(now));
@@ -37,11 +39,17 @@ const initTransition = () => {
     return ;
   }
     button.addEventListener('click', (e) => {
+      setTimeout(function(){ console.log("toto"); }, 5000);
+
       greenButton.classList.add("transparent");
       // footer.classList.add("transparent");
       curtain.classList.add("curtain-up");
-      boussole.classList.add("tourne");
-      incrementor();
+      // boussole.classList.add("tourne");
+      setTimeout(function(){ boussole.classList.add("tourne"); }, 1000);
+      setTimeout(function(){ console.log("toto"); rowIti.classList.remove("transparent"); incrementor('#itinerary-count');}, 1200);
+      setTimeout(function(){ console.log("toto"); rowBasecamp.classList.remove("transparent"); incrementor('#basecamp-count');}, 5000);
+      setTimeout(function(){ console.log("toto"); rowMassif.classList.remove("transparent"); incrementor('#massif-count'); }, 9000);
+
   });
 }
 
