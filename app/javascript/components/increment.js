1726d8f64b6755789f9913437e760bb29c57e610
nicolas <nicolas.eymenier@gmail.com>
2019-03-04 10:39:36 +0100

    transition reliée

--- app/javascript/components/increment.js
+++ app/javascript/components/increment.js
@@ -1,22 +1,12 @@
 import $ from 'jquery';
 
-const button = document.querySelector('#test-transition')
+const form = document.querySelector('#new_trip')
 const boussole = document.querySelector('.boussole')
 const curtain = document.querySelector('.curtain')
-const footer = document.querySelector('.page-footer')
-const greenButton = document.querySelector('#new_trip')
 const rowIti = document.querySelector('#row-iti')
 const rowMassif = document.querySelector('#row-massif')
 const rowBasecamp = document.querySelector('#row-basecamp')
 
-function sleep(milliseconds) {
-  var start = new Date().getTime();
-  for (var i = 0; i < 1e7; i++) {
-    if ((new Date().getTime() - start) > milliseconds){
-      break;
-    }
-  }
-}
 
 
 const incrementor = (id) => {
@@ -24,7 +14,7 @@ const incrementor = (id) => {
     $(this).prop('Counter',0).animate({
         Counter: $(this).text()
     }, {
-        duration: 4000,
+        duration: 3000,
         easing: 'swing',
         step: function (now) {
             $(this).text(Math.ceil(now));
@@ -35,20 +25,18 @@ const incrementor = (id) => {
 
 
 const initTransition = () => {
-  if (button == null) {
+  if (form == null) {
     return ;
   }
-    button.addEventListener('click', (e) => {
-      setTimeout(function(){ console.log("toto"); }, 5000);
-
-      greenButton.classList.add("transparent");
-      // footer.classList.add("transparent");
+    form.addEventListener('submit', (e) => {
+      e.preventDefault();
+      form.classList.add("transparent");
       curtain.classList.add("curtain-up");
-      // boussole.classList.add("tourne");
       setTimeout(function(){ boussole.classList.add("tourne"); }, 1000);
-      setTimeout(function(){ console.log("toto"); rowIti.classList.remove("transparent"); incrementor('#itinerary-count');}, 1200);
-      setTimeout(function(){ console.log("toto"); rowBasecamp.classList.remove("transparent"); incrementor('#basecamp-count');}, 5000);
-      setTimeout(function(){ console.log("toto"); rowMassif.classList.remove("transparent"); incrementor('#massif-count'); }, 9000);
+      setTimeout(function(){ rowIti.classList.remove("transparent"); incrementor('#itinerary-count');}, 1200);
+      setTimeout(function(){ rowBasecamp.classList.remove("transparent"); incrementor('#basecamp-count');}, 4200);
+      setTimeout(function(){ rowMassif.classList.remove("transparent"); incrementor('#massif-count'); }, 4200);
+      setTimeout(function(){ form.submit(); }, 7200);
 
   });
 }
