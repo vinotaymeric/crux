4b4404974e209ff54fb03528cfc43d3b8ff345f3
jordi Tabit <jordi.tabit@hotmail.fr>
2019-03-04 12:38:28 +0100

    fixed avalanche display + slide between tabs

--- app/javascript/packs/init_driving_time_on_trips.js
+++ app/javascript/packs/init_driving_time_on_trips.js
@@ -1,23 +1,24 @@
 
 const initDrivingTimeOnTrips = () => {
-  const cards = document.querySelectorAll('.card');
 
-  const addDrivingTimeToCard = (card, drivingTime) => {
-      card.querySelector('#driving-time').insertAdjacentHTML('beforeEnd', `<i class="fas fa-car"></i> ${drivingTime}`);
-  };
+const addDrivingTimeToCard = (card, drivingTime) => {
+    card.querySelector('#driving-time').insertAdjacentHTML('beforeEnd', `<i class="fas fa-car"></i> ${drivingTime}`);
+};
 
-  const fetchDrivingTime = (ori_lat, ori_long, desti_lat, desti_long) => {
-    const apiKey='5b3ce3597851110001cf624811f828bf686741448a9ed982b4169a87'
-    const url = `https://api.openrouteservice.org/matrix?api_key=${apiKey}&profile=driving-car&locations=${ori_long},${ori_lat}%7C${desti_long},${desti_lat}`;
-    return fetch(url).then(response => response.json()).then(data => data.durations[0][1] || []);
-  };
+const fetchDrivingTime = (ori_lat, ori_long, desti_lat, desti_long) => {
+  const apiKey='5b3ce3597851110001cf624811f828bf686741448a9ed982b4169a87'
+  const url = `https://api.openrouteservice.org/matrix?api_key=${apiKey}&profile=driving-car&locations=${ori_long},${ori_lat}%7C${desti_long},${desti_lat}`;
+  return fetch(url).then(response => response.json()).then(data => data.durations[0][1] || []);
+};
 
-  const convertSecondsToHoursMins = (seconds) => {
-    const hours = Math.floor(seconds / 3600);
-    const remainingSeconds = seconds % 3600;
-    const minutes = Math.round(remainingSeconds / 60);
-    return (seconds >= 3600) ? `${hours}h${minutes}` : `${minutes}min`;
-  }
+const convertSecondsToHoursMins = (seconds) => {
+  const hours = Math.floor(seconds / 3600);
+  const remainingSeconds = seconds % 3600;
+  const minutes = Math.round(remainingSeconds / 60);
+  return (seconds >= 3600) ? `${hours}h${minutes}` : `${minutes}min`;
+}
+
+  const cards = document.querySelectorAll('.card-basecamp-activity');
 
   cards.forEach((card) => {
     const cardContent = card.querySelector('.card-content');
