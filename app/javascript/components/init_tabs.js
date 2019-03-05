afb52d76c2dd283d123f6db6ec816492763083e5
jordi Tabit <jordi.tabit@hotmail.fr>
2019-03-05 12:51:24 +0100

    fixed bug with slider

--- app/javascript/components/init_tabs.js
+++ app/javascript/components/init_tabs.js
@@ -1,6 +1,10 @@
 const initTabs = () => {
   const tabs = document.querySelector('.tabs');
 
+  const changeTabHeight = () => {
+
+  };
+
   if (tabs) {
     M.Tabs.init(tabs, {
       swipeable: true,
@@ -12,7 +16,18 @@ const initTabs = () => {
         document.querySelector(".tabs-content").style.height = height + 'px';
       }
     });
+    window.addEventListener('resize', function(event) {
+          const tab = document.querySelector(".carousel-item.active");
+          const tabsContent = document.querySelector(".tabs-content");
+          let height = 0;
+          if (tab.children[0]) {
+            height = tab.children[0].scrollHeight + 20;
+          }
+          tabsContent.style.height = height + 'px';
+          location.reload(true);
+    });
   };
+
 };
 
 export default initTabs;
