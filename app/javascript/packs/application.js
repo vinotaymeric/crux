753fed70255114620d4dc91f45878e4a7e919f81
Aymeric Vinot <aymeric.vinot@gmail.com>
2019-03-05 11:10:59 +0100

    clean new trip form js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -29,9 +29,8 @@ const activities = document.querySelectorAll('#user_activity_level');
 
 activities.forEach( (element) => {
   element.addEventListener('click', (e) => {
-    e.preventDefault();
-    const value = e.currentTarget.value;
     e.currentTarget.closest('form').submit();
+    e.preventDefault();
   });
 });
 
