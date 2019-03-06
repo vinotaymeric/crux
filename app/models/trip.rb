ca7203e2f9d83b8401fd9d1931a0a61e5e5cea1d
Aymeric Vinot <aymeric.vinot@gmail.com>
2019-03-06 14:56:48 +0100

    bugfix trip validation

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -4,6 +4,7 @@ class Trip < ApplicationRecord
   has_many :basecamps_activities, through: :trips_basecamps_activities
   has_many :favorite_itineraries
   has_many :itineraries, through: :favorite_itineraries
+  attribute :validated, default: false
   validates :start_date, presence: true
   validates :end_date, presence: true
   geocoded_by :location, latitude: :coord_lat, longitude: :coord_long
