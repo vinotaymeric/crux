66cef447ca9522e064c4b9539c998826fd5ca9ff
jordi Tabit <jordi.tabit@hotmail.fr>
2019-03-06 14:25:53 +0100

    Remove reload on resize window

--- app/javascript/components/init_tabs.js
+++ app/javascript/components/init_tabs.js
@@ -19,16 +19,6 @@ const initTabs = () => {
         console.log(tab.children[0].scrollHeight);
       }
     });
-    window.addEventListener('resize', function(event) {
-          const tab = document.querySelector(".carousel-item.active");
-          const tabsContent = document.querySelector(".tabs-content");
-          let height = 0;
-          if (tab.children[0]) {
-            height = tab.children[0].scrollHeight + 20;
-          }
-          tabsContent.style.height = height + 'px';
-          location.reload(true);
-    });
   };
 
 };
