ce4071e0a04524b7b1591efa5ad5873f0deca087
Aymeric Vinot <aymeric.vinot@gmail.com>
2019-03-07 18:01:49 +0100

    c

--- config/initializers/algoliasearch.rb
+++ config/initializers/algoliasearch.rb
@@ -1,1 +1,1 @@
-AlgoliaSearch.configuration = { application_id: 'L83PWH35UP', api_key: '714d73dd009064dca297515f592487e2' }
+AlgoliaSearch.configuration = { application_id: 'L83PWH35UP', api_key: ENV['ALGOLIA_API_KEY'] }
