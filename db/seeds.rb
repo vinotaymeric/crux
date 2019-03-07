27b8c0a630c76c796e36eaf3fa6113fbf7e11f56
Aymeric Vinot <aymeric.vinot@gmail.com>
2019-03-07 13:05:45 +0100

    menu : added nouvelle sortie

--- db/seeds.rb
+++ db/seeds.rb
@@ -78,47 +78,39 @@
 #   Weather.create!(basecamp: basecamp_activity.basecamp)
 # end
 
-# def api_call(lat, lon)
-#   url = "https://api.apixu.com/v1/forecast.json?key=3a0aef724b764f6cb35161705192702&q=#{lat},#{lon}&days=7"
-#   JSON.parse(open(url).read)
-# end
+def api_call(lat, lon)
+  url = "https://api.apixu.com/v1/forecast.json?key=3a0aef724b764f6cb35161705192702&q=#{lat},#{lon}&days=7"
+  JSON.parse(open(url).read)
+end
 
-# Weather.all.each do |weather|
-#   begin
-#   basecamp = weather.basecamp
-#   weather_hash = api_call(basecamp.coord_lat, basecamp.coord_long)
-#   p weather_hash
-#   score = 0
+Weather.all.each do |weather|
+  begin
+  basecamp = weather.basecamp
+  weather_hash = api_call(basecamp.coord_lat, basecamp.coord_long)
+  weather.forecast = weather_hash["forecast"]["forecastday"]
+  weather.save!
 
-#   weather_hash["forecast"]["forecastday"].each do |day|
-#     day_hash = day["day"]
-#     score += day_hash["avgtemp_c"].to_i + day_hash["avgvis_km"].to_i - day_hash["maxwind_kph"].to_i - day_hash["totalprecip_mm"].to_i
-#   end
-#   weather.weekend_score = score
-#   weather.forecast = weather_hash["forecast"]["forecastday"]
-#   weather.save!
+  # Adding a rescue so that it works even a call fails
 
-#   # Adding a rescue so that it works even a call fails
+  rescue Exception => e
+  puts "#weather {weather.id} a pété"
+  end
 
-#   rescue Exception => e
-#   puts "#weather {weather.id} a pété"
-#   end
-
-# end
+end
 
 # # Update iti levels
 
-Itinerary.all.each do |itinerary|
-  expérimenté = ["TD-", "TD", "TD+", "ED-", "ED", "ED+"]
-  intermediaire = ["D-", "D", "D+", "AD-", "AD", "AD+"]
-  débutant = ["PD-", "PD", "PD+", "F-", "F", "F+"]
-  if expérimenté.include?(itinerary.difficulty)
-    itinerary.level = "Expérimenté"
-  elsif intermediaire.include?(itinerary.difficulty)
-    itinerary.level = "Intermédiaire"
-  elsif débutant.include?(itinerary.difficulty)
-    itinerary.level = "Débutant"
-  end
-  itinerary.save!
-  p itinerary.id
-end
+# Itinerary.all.each do |itinerary|
+#   expérimenté = ["TD-", "TD", "TD+", "ED-", "ED", "ED+"]
+#   intermediaire = ["D-", "D", "D+", "AD-", "AD", "AD+"]
+#   débutant = ["PD-", "PD", "PD+", "F-", "F", "F+"]
+#   if expérimenté.include?(itinerary.difficulty)
+#     itinerary.level = "Expérimenté"
+#   elsif intermediaire.include?(itinerary.difficulty)
+#     itinerary.level = "Intermédiaire"
+#   elsif débutant.include?(itinerary.difficulty)
+#     itinerary.level = "Débutant"
+#   end
+#   itinerary.save!
+#   p itinerary.id
+# end
