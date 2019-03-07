98464824c9608bc04f10e78c6ace6448c49f11ee
Aymeric Vinot <aymeric.vinot@gmail.com>
2019-03-07 16:57:51 +0100

    scraping recent iti

--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -17,5 +17,33 @@ class Itinerary < ApplicationRecord
   def small?
     ObjectSpace.memsize_of(content.to_json) < 10000
   end
-  #WillPaginate.per_page = 10
+
+  def api_call(itinerary, id)
+    url = "https://api.camptocamp.org/#{itinerary}/#{id.to_s}"
+    begin
+    JSON.parse(open(url).read)
+    rescue Exception => e
+    end
+  end
+
+  def recent_conditions
+    itinerary = Addressable::URI.encode_component(self.name, Addressable::URI::CharacterClasses::QUERY)
+    url_c2C = "https://www.google.fr/search?ei=8gmBXLvLGOCCjLsPsf2_gAo&q=site%3Ahttps%3A%2F%2Fwww.camptocamp.org%2Froutes%2F+#{itinerary}+&oq=site%3Ahttps%3A%2F%2Fwww.camptocamp.org%2Froutes%2F+#{itinerary}"
+
+    html_file_c2C = open(url_c2C).read
+    html_doc_c2c = Nokogiri::HTML(html_file_c2C)
+
+    id = html_doc_c2c.search('h3').first.children.attribute('href').value.split("/")[5].to_i
+
+    outing_ids = []
+    return if api_call("outings", id).nil?
+    api_call("outings", id)["associations"]["recent_outings"]["documents"].each do |outing|
+      date = Date.parse outing["date_end"]
+      if date.upto(Date.today).to_a.size < 60
+        outing_ids << [outing["document_id"].to_i, outing["date_end"].to_s]
+      end
+    end
+    outing_ids
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,28 +2,12 @@ require 'json'
 require 'open-uri'
 require 'nokogiri'
 require 'addressable/uri'
+require 'date'
 
 
-def api_call(itinerary, id)
-  url = "https://api.camptocamp.org/#{itinerary}/#{id.to_s}"
-  p url
-  JSON.parse(open(url).read)
-end
 
-def recent_conditions(itinerary)
-  itinerary = Addressable::URI.encode_component(itinerary.name, Addressable::URI::CharacterClasses::QUERY)
-  url_c2C = "https://www.google.fr/search?ei=8gmBXLvLGOCCjLsPsf2_gAo&q=site%3Ahttps%3A%2F%2Fwww.camptocamp.org%2Froutes%2F+#{itinerary}+&oq=site%3Ahttps%3A%2F%2Fwww.camptocamp.org%2Froutes%2F+#{itinerary}"
 
-  html_file_c2C = open(url_c2C).read
-  html_doc_c2c = Nokogiri::HTML(html_file_c2C)
-
-  id = html_doc_c2c.search('h3').first.children.attribute('href').value.split("/")[5].to_i
-
-  p api_call("outings", id)["associations"]["recent_outings"]["total"]
-end
-
-recent_conditions(Itinerary.find(347))
-
+p Itinerary.find(382).recent_conditions
 
 # url_skitour = "https://www.google.fr/search?ei=PQ2BXO3vKo3jgweYlavQCA&q=site%3Ahttp%3A%2F%2Fwww.skitour.fr%2Ftopos%2F+#{itinerary}&oq=site%3Ahttp%3A%2F%2Fwww.skitour.fr%2Ftopos%2F+#{itinerary}"
 # html_file_skitour = open(url_skitour).read
