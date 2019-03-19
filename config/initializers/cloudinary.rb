8a16f8e9d99d44e3054a30ceea87c661cbbca1b0
Aymeric Vinot <aymeric.vinot@gmail.com>
2019-03-19 16:55:34 +0100

    css

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,5 +1,5 @@
 Cloudinary.config do |config|
-  config.cloud_name = 'deesjjv4h'
+  config.cloud_name = 'dbehokgcg'
   config.api_key = ENV['API_KEY_CLOUDINARY']
   config.api_secret = ENV['API_SECRETE_CLOUDINARY']
   config.secure = true
