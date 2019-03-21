f76e831da9756020a29ba133229740954be2b62c
Aymeric Vinot <aymeric.vinot@gmail.com>
2019-03-21 12:47:04 +0100

    cleaned su form

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -2,7 +2,13 @@ class RequestsController < ApplicationController
   def create
     redirect_to new_user_registration_path and return if current_user.nil?
     trip = Trip.find(params[:trip_id])
-    Request.create!(trip: trip)
-    flash[:notice] = 'On se met au travail, et on revient vite vers toi.'
+
+    if Request.find_by(trip_id: trip.id).nil?
+      Request.create!(trip: trip)
+      flash[:notice] = "On se met au travail, et on revient vite vers toi."
+    else
+      flash[:notice] = "On fait au plus vite, détends-toi ;)"
+    end
+    redirect_back(fallback_location: root_path)
   end
 end
