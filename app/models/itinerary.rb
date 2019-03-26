894258201dae1c54c81bec26ba54707a05d2402f
Aymeric Vinot <aymeric.vinot@gmail.com>
2019-03-26 09:16:19 +0100

    small pictures

--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -100,7 +100,7 @@ class Itinerary < ApplicationRecord
 
   def picture_url_p
     placeholder = "https://res.cloudinary.com/dbehokgcg/image/upload/v1553511342/placeholder.png"
-    self.picture_url.nil? ? placeholder : self.picture_url
+    self.picture_url.nil? ? placeholder : "#{self.picture_url[0..-5]}MI.jpg"
   end
 
   def outing_months
