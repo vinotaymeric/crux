bc64703fa55ed7150b6e4aac6e367dfea4b6a72c
Aymeric Vinot <aymeric.vinot@gmail.com>
2019-03-27 12:37:48 +0100

    better search

--- app/javascript/packs/itinerary_search.js
+++ app/javascript/packs/itinerary_search.js
@@ -1,16 +1,30 @@
+const searchResults = document.querySelector("#search-results");
+const searchBar = document.querySelector('#query');
+
+function addTextAreaCallback(textArea, callback, delay) {
+    var timer = null;
+    textArea.onkeyup = function() {
+        if (timer) {
+            window.clearTimeout(timer);
+        }
+        timer = window.setTimeout( function() {
+            timer = null;
+            if(callback) callback();
+        }, delay );
+    };
+    textArea = null;
+}
+
+
+const search = (textArea) => {
+  Rails.fire(textArea.closest('form'), 'submit');
+  if (textArea.value.length < 2) {searchResults.innerHTML = ""};
+};
 
 const initItinerarySearch = () => {
-  const searchResults = document.querySelector("#search-results");
-  const searchBar = document.querySelector('#query');
   if (searchBar === null) {return}
-
-  searchBar.addEventListener('keyup', (e) => {
-    // console.log(e.currentTarget.value.length);
-    // console.log(e.currentTarget.value.length);
-    Rails.fire(e.currentTarget.closest('form'), 'submit');
-    // if (e.currentTarget.value.length < 2) {console.log('youhou')};
-    if (e.currentTarget.value.length < 2) {searchResults.innerHTML = ""};
-  });
+  addTextAreaCallback( searchBar, search(searchBar), 1000 );
 }
 
 export default initItinerarySearch;
+
