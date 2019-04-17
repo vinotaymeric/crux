b47d8e72f1928f0e57034bf6463ab1531c17b42a
Aymeric Vinot <aymeric.vinot@gmail.com>
2019-04-17 11:47:07 +0200

    dependent destroy on user

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -3,7 +3,7 @@ class Trip < ApplicationRecord
   belongs_to :city, optional: true
   belongs_to :user_activity, optional: true
   has_many :favorite_itineraries, dependent: :destroy
-  has_many :itineraries, through: :favorite_itineraries
+  has_many :itineraries, through: :favorite_itineraries, dependent: :destroy
   has_many :trip_activities, dependent: :destroy
   has_one :trip_activity, dependent: :destroy
   attribute :validated, default: false
