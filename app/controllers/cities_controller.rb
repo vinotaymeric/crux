cc3ca70d5672674485f423f8720bb1e0f7514919
Aymeric Vinot <aymeric.vinot@gmail.com>
2019-06-05 14:20:14 +0200

    test

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,78 +1,14 @@
-class CitiesController < ApplicationController
+def index
+  @users = User.where(native_language: params[:native_language]) unless params[:native_language].blank?
 
-  def index
-    @user = current_or_guest_user
-    @trip = Trip.find(params[:trip_id])
-    @one_hour_polygon = @trip.isochrone_coordinates
-    find_top_cities
-
-    # Mapbox
-    @markers = @cities.map do |city|
-      {
-        lng: city.coord_long,
-        lat: city.coord_lat,
-        infoWindow: render_to_string(partial: "infowindow", locals: {city: city, trip: @trip}),
-        image_url: "https://res.cloudinary.com/dbehokgcg/image/upload/v1558626248/crux/images/#{city.temp_activity}.png"
-      }
-
-    end
-    # Add trip start location
-    @markers << {
-        lng: @trip.coord_long,
-        lat: @trip.coord_lat,
-        image_url: helpers.asset_url('https://cdn1.iconfinder.com/data/icons/business-sets/512/target-512.png'),
-    }
-  end
-
-  private
-
-  def score(city, trip)
-    # Compute subscores
-    itinerary_score = [city.nb_good_itineraries - 1, 0].max / 2
-    weather_score = city.weather.score(trip.start_date, trip.end_date)
-
-    if city.included_in_polygon?(@one_hour_polygon)
-      distance_score = 1
+  unless params[:tag_id].blank?
+    if params[:tag_id].class == Integer
+      @users = @users.joins(:taggings).where(taggings: {tag_id: params[:tag_id]})
+      @users = @users.all.uniq.select { |u| (u.tags.ids & params[:tag_id].map(&:to_i)).size == params[:tag_id].size }
     else
-      distance_score = 1 + trip.distance_from(city) / 50
-    end
-    # Return final score
-    ([weather_score, [itinerary_score, trip.duration].min].min) / distance_score ** (1.05 - trip.duration / 20)
-  end
-
-  def define_city_activities
-    @trip.trip_activities.map do |trip_activity|
-      next if trip_activity.level == "Niveau ?" || trip_activity.level.nil?
-      activity = trip_activity.activity
-
-      city_activity = City.select("cities.*, SUM(itineraries.score) as nb_good_itineraries_from_query, COUNT(itineraries.id) as nb_itineraries_from_query")
-      .joins(:itineraries)
-      .where(itineraries: {activity_id: activity.id, universal_difficulty: trip_activity.level.downcase })
-      .group("cities.id HAVING SUM(itineraries.score) > 0")
-      .order("nb_good_itineraries_from_query")
-
-      city_activity.each do |city|
-        city.temp_activity = activity.name
-        city.nb_itineraries = city.nb_itineraries_from_query
-        city.nb_good_itineraries = city.nb_good_itineraries_from_query
-      end
+      @tag = Tag.where(name: params[:tag_id]).first
+      @users = @users.joins(:taggings).where(taggings: {tag_id: @tag.id})
     end
   end
 
-  def find_top_cities
-    if session[:cities_activities].present?
-      @cities = SessionStorage.new(session[:cities_activities]).convert_to_objects
-    else
-      cities_activities = define_city_activities.flatten!
-      # Order by score
-      cities_activities.sort_by! do |city_activity|
-        city_activity.temp_score = score(city_activity, @trip)
-        [city_activity.temp_score, - @trip.distance_from(city_activity)]
-      end
-      # Take only top 14 and send them to the view
-      @cities = cities_activities.reverse[0..13]
-      # Store them in session to avoid long reloads
-      session[:cities_activities] = SessionStorage.new(@cities).convert_to_hashes
-    end
-  end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -69,4 +69,87 @@ const buttonTabs = () => {
 
 buttonTabs();
 
-console.log(gon.user.email)
+// console.log(gon.user.email)
+
+// function myfun(){
+//      // Write your business logic here
+//      console.log('tti');
+// }
+
+// button = document.querySelector('.btn-large')
+
+// window.onbeforeunload = function(){
+//   myfun();
+//   // button.click();
+//   return 'Are you sure you want to leave?';
+// };
+
+// let finished = false
+// let test = 'toto'
+
+// function sleep(milliseconds) {
+//   var start = new Date().getTime();
+//   for (var i = 0; i < 1e7; i++) {
+//     if ((new Date().getTime() - start) > milliseconds){
+//       break;
+//     }
+//   }
+// }
+
+// setTimeout(function(){finished = true; }, 3000);
+
+// while (finished === false) {
+//   console.log('tti');
+//   sleep(1000);
+// }
+
+const initStars = () => {
+   var width = $(window).width();
+   var height = $(window).height();
+   var star_count = 250;
+
+   var stars = new Array;
+   let i
+   for ( i=1; i<= star_count; i++ ) {
+
+     var rand_x =  Math.floor((Math.random()*width)+1);
+     var rand_y =  Math.floor((Math.random()*height)+1);
+     var rand_color = Math.floor((Math.random()*10)+1);
+     var speed = Math.floor((Math.random()*5)+1);
+
+
+     var element = document.createElement('div');
+     element.className = "star star-" + rand_color;
+     element.style.left = rand_x + "px";
+     element.style.top = rand_y + "px";
+     element.setAttribute("data-speed", speed);
+     document.body.appendChild( element );
+
+     stars[i] = element;
+
+   }
+
+   var interval = setInterval(function(){
+
+     for ( i=1; i<stars.length; i++ ) {
+
+       var left = stars[i].style.left;
+       if ( left === "0px" ) {
+         stars[i].style.left = (width - 10) + "px";
+       } else {
+         stars[i].style.left = parseInt( left ) - stars[i].getAttribute("data-speed") + "px";
+       }
+     }
+   },1000/60);
+
+   setTimeout(function() {
+    stars = document.querySelectorAll('p')
+
+    stars.forEach(function(element) {
+      element.style.visibility = "hidden";
+    });
+   }, 5000)
+}
+
+
+initStars()
